ec1d109f308e541953e8d3b66064bfce49337a90
tanshuai <1432753451@qq.com>
2018-11-08 18:36:14 +0800

    Python分析拉钩岗位信息

---
+++ data_analysis.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : tanshuai
+# @Contact  : tyh9436@gmail.com
+# @Software : PyCharm
+# @File     : data_analysis.py
+# @Time     : 2018/11/6 12:17
+
+import pandas as pd
+import matplotlib.pyplot as plt # 导入模块报错解决 https://www.cnblogs.com/harelion/p/5637767.html
+from wordcloud import WordCloud
+from scipy.misc import imread
+import jieba
+from pylab import mpl
+import sys
+
+# 使matplotlib模块能显示中文
+mpl.rcParams['font.sans-serif'] = ['Arial Unicode MS'] # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
+
+# 1、读取数据
+filename = '运维工程师'
+filename = 'dba'
+filename = 'Python开发工程师'
+filename = 'Python开发'
+# filename = '运维开发工程师'
+# filename = 'lagou_jobs'
+# df = pd.read_csv('lagou_jobs.csv', encoding='utf-8')
+df = pd.read_csv(filename + '.csv', encoding='utf-8')
+
+# 2、计算薪水, 将字符串转化为列表, 再取区间的前25%, 比较贴近现实, 由于CSV文件内的数据是字符串形式,先用正则表达式将字符串转化为列表
+pattern = '\d+'
+df['salary'] = df['工资'].str.findall(pattern)
+
+avg_salary = []
+for k in df['salary']:
+    int_list = [int(n) for n in k]
+    avg_wage = int_list[0]+(int_list[1]-int_list[0])/4
+    avg_salary.append(avg_wage)
+df['月工资'] = avg_salary
+# 2.1 将清洗后的数据保存,以便检查
+df.to_csv('draft.csv', index=False)
+# 2.2 描述统计
+print('岗位工资描述：\n{}'.format(df['月工资'].describe()))
+
+# '''
+# 3、绘制频率直方图并保存
+plt.hist(df['月工资'], bins = 12) # bins指定画多大的格子
+plt.xlabel('工资 (千元)')
+plt.ylabel('次数')
+plt.title(filename + "工资直方图")
+plt.savefig('histogram.jpg')
+plt.show()
+# '''
+
+
+# '''
+# 4、绘制饼图并保存
+count = df['区域'].value_counts()
+plt.pie(count, labels=count.keys(), labeldistance=1.4, autopct='%2.1f%%')
+plt.axis('equal')  # 使饼图为正圆形
+plt.title(filename + "岗位地区分布图")
+plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
+plt.savefig('pie_chart.jpg')
+plt.show()
+# '''
+
+# '''
+# 5、绘制词云,将职位福利中的字符串汇总
+text = ''
+for line in df['职位福利']:
+   text += line
+   
+# 5.1 使用jieba模块将字符串分割为单词列表
+cut_text = ' '.join(jieba.cut(text)) # 字符串分词
+cloud = WordCloud(
+       font_path='Arial Unicode.ttf',
+       background_color='white', # 背景设置成(white)白色
+       mask=imread('cloud.jpg'), #设置背景图
+       max_words=1000,
+       max_font_size=100
+       )
+
+word_cloud = cloud.generate(cut_text)
+# 5.2 保存词云图片
+word_cloud.to_file('word_cloud.jpg')
+plt.imshow(word_cloud)
+plt.axis('off')
+plt.show()
+# '''
---
+++ getinfo_spider.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : tanshuai
+# @Contact  : tyh9436@gmail.com
+# @Software : PyCharm
+# @File     : getinfo.py
+# @Time     : 2018/11/6 11:43
+
+import requests
+import math
+import pandas as pd
+import time
+import sys
+
+def get_json(url, num): # 接收两个参数：地址、页数
+    '''从网页获取JSON,使用POST请求,加上头部信息'''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+        'Host': 'www.lagou.com',
+       'Referer':'https://www.lagou.com/jobs/list_python%E5%BC%80%E5%8F%91?labelWords=&fromSearch=true&suginput=',
+        'X-Anit-Forge-Code': '0',
+       'X-Anit-Forge-Token': 'None',
+       'X-Requested-With': 'XMLHttpRequest'
+    }
+
+    data = {
+        'first': 'true',
+        'pn': num,
+        'kd': 'Python开发'}
+    res = requests.post(url, headers=headers, data=data)
+    res.raise_for_status()
+    res.encoding = 'utf-8'
+    # 得到包含职位信息的字典
+    page = res.json()
+    return page
+
+
+def get_page_num(count):
+    '''''计算要抓取的页数'''
+    # 每页15个职位,向上取整
+    res = math.ceil(count/15)
+    # 拉勾网最多显示30页结果
+    if res > 30:
+        return 30
+    else:
+        return res
+
+
+def get_page_info(jobs_list):
+    '''''对一个网页的职位信息进行解析,返回列表'''
+    page_info_list = []
+    for i in jobs_list:
+        job_info = []
+        job_info.append(i['companyFullName'])       # 公司全名
+        job_info.append(i['companyShortName'])      # 公司简称
+        job_info.append(i['companySize'])           # 公司规模
+        job_info.append(i['financeStage'])          # 融资阶段
+        job_info.append(i['district'])              # 区域
+        job_info.append(i['positionName'])          # 职位名称
+        job_info.append(i['workYear'])              # 工作经验
+        job_info.append(i['education'])             # 学历要求
+        job_info.append(i['salary'])                # 工资
+        job_info.append(i['positionAdvantage'])     # 职位福利
+        page_info_list.append(job_info)
+    return page_info_list
+
+
+def main():
+    # 第一步：确定获取数据的URL
+    url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E5%8C%97%E4%BA%AC&needAddtionalResult=false'
+    # 第二步：请求数据，获取总的职位数
+    page_one = get_json(url, 1) # 先设定页数为1,获取总的职位数
+    total_count = page_one['content']['positionResult']['totalCount']
+    # 2.1 根据总的职位数，根据每页15个职位数分割，最后得到请求的次数
+    num = get_page_num(total_count)
+    total_info = []   # 存储所有抓取的数据
+    # 2.2 第一次请求后暂停20秒，防止被封；
+    time.sleep(20)
+    print('职位总数:{},页数:{}'.format(total_count, num))
+
+    for n in range(1, num+1):
+        # 对每个网页读取JSON, 获取每页数据
+        page = get_json(url, n)
+        jobs_list = page['content']['positionResult']['result']
+        page_info = get_page_info(jobs_list)
+        total_info += page_info
+        print('已经抓取第{}页, 职位总数:{}'.format(n, len(total_info)))
+        # 每次抓取完成后,暂停一会,防止被服务器拉黑
+        time.sleep(30)
+    # 将总数据转化为data frame再输出
+    df = pd.DataFrame(data=total_info, columns=['公司全名','公司简称','公司规模','融资阶段','区域','职位名称','工作经验','学历要求','工资','职位福利'])
+    df.to_csv('lagou_jobs.csv', index=False)
+    print('已保存为csv文件.')
+
+if __name__ == "__main__":
+    main()
